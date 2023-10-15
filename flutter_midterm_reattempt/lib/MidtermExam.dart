8e0c450e1e4a125042c03fd22f2428e9fc62f3a8
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-15 10:42:19 +0500

    List to ban gayi

--- flutter_midterm_reattempt/lib/MidtermExam.dart
+++ flutter_midterm_reattempt/lib/MidtermExam.dart
@@ -91,8 +91,59 @@ ListView _buildingView(List<Products> itemsList)
     {
       return Card
       (
-        color: Colors.yellow,
-        child: Text(itemsList[i].title),
+        color: Colors.white60,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Column
+        (
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: 
+          [
+            Container
+            (
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration
+              (
+                // borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+                // borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Image.network
+              (
+                itemsList[i].thumbnail.toString(), 
+                fit: BoxFit.fill,
+              ),
+            ),
+            Padding
+            (
+              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+              child: Row
+              (
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: 
+                [
+                  Text(itemsList[i].title),
+                  Row
+                  (
+                    children: 
+                    [
+                      Text(itemsList[i].price.toString() + " USD"),
+                      IconButton
+                      (
+                        icon: Icon(Icons.remove_red_eye_sharp),
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding
+            (
+              padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+              child: Text(itemsList[i].description.toString()),
+            ),
+          ],
+        ),
       );
       // return Card
       // (
