aff736aad2be7f627ae939dfcd306e98b1556f7e
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2023-10-15 10:19:22 +0500

    Boss is bossing

--- flutter_midterm_reattempt/lib/MidtermExam.dart
+++ flutter_midterm_reattempt/lib/MidtermExam.dart
@@ -91,189 +91,194 @@ ListView _buildingView(List<Products> itemsList)
     {
       return Card
       (
-        color: Colors.white60,
-        shape: RoundedRectangleBorder
-        (
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        // child: Padding
-        // (
-        //   padding: EdgeInsets.all(10.0),
-          
-          child: Column
-          (
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: 
-            [
-              Expanded
-              (
-                child: Container
-                (
-                  alignment: Alignment.topCenter,
-                  width: double.infinity,
-                  // height: double.minPositive,
-                  // height: 50,
-                  decoration: BoxDecoration
-                  (
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Image.network
-                  (
-                    itemsList[i].thumbnail.toString(),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              // Expanded
-              // (
-              //   child: Image.network
-              //   (
-              //     itemsList[i].thumbnail.toString(),
-              //     fit: BoxFit.fitWidth,
-              //   ),
-              // ),
-
-              //  Expanded
-              // (
-              //   child: Container
-              //   (
-              //     width: double.infinity,
-              //     decoration: BoxDecoration
-              //     (
-              //       borderRadius: BorderRadius.circular(20.0),
-              //     ),
-              //     child: Image.network
-              //     (
-              //       itemsList[i].thumbnail.toString(),
-              //       fit: BoxFit.fill,
-              //     ),
-              //   ),
-              // ),
-              Padding
-              (
-                padding: EdgeInsets.only(left:10.0, right: 10.0),
-                child:
-                Row
-                (
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: 
-                  [
-                    
-                    // Text(itemsList[i].title, style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text(itemsList[i].title.length > 20 ? '${itemsList[i].title.substring(0, 20)}...' : itemsList[i].title, style: TextStyle(fontWeight: FontWeight.bold)),
-                    Row
-                    (
-                      children: 
-                      [
-                        Text(itemsList[i].price.toString() + " USD", style: TextStyle(fontWeight: FontWeight.bold)),
-                        IconButton
-                        (
-                          icon: Icon(Icons.remove_red_eye_sharp),
-                          onPressed: ()
-                          {
-                            showModalBottomSheet<void>
-                            (
-                            
-                              shape: RoundedRectangleBorder
-                              (
-                                borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
-                              ),
-                              constraints: BoxConstraints
-                              (
-                                // minWidth: double.infinity,
-                              ),
-                              // isScrollControlled: true,
-                              context: context,
-                              builder: (BuildContext context)
-                              {
-                                return Column
-                                (
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: 
-                                  [
-                                    Container
-                                    (
-                                      margin: const EdgeInsets.symmetric(vertical: 20),
-                                      height: 150,
-                                      child: ListView
-                                      (
-                                        scrollDirection: Axis.horizontal,
-                                        children: itemsList[i].images != null && itemsList[i].images.isNotEmpty ? itemsList[i].images.map((imageInList)
-                                        {
-                                          return Card
-                                          (
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
-                                            child: Container
-                                            (
-                                              decoration: BoxDecoration
-                                              (
-                                                borderRadius: BorderRadius.circular(20.0),
-                                              ),
-                                              child: Image.network(imageInList, fit: BoxFit.fitHeight, width: 150),
-                                            ),
-                                          );
-                                          // return Image.network(imageInList);
-                                        }).toList() : <Widget>[],
-                                      ),
-                                    ),
-                                    ListTile
-                                    (
-                                      title: Text(itemsList[i].title,style: TextStyle(fontWeight: FontWeight.bold)),
-                                      subtitle: Text(itemsList[i].description.toString()),
-                                    ),
-                                    ListTile
-                                    (
-                                      title: Text("\$" + itemsList[i].price.toString(),style: TextStyle(fontWeight: FontWeight.bold)),
-                                    ),
-                                    ListTile
-                                    (
-                                      title: Row
-                                      (
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: 
-                                        [
-                                          Row
-                                          (
-                                            children: 
-                                            [
-                                              Icon(Icons.star, color: Colors.yellow,),
-                                              Text(itemsList[i].rating.toString(),),   
-                                            ],  
-                                          ),
-                                          Row
-                                          (
-                                            children: 
-                                            [
-                                              Text(itemsList[i].discountPercentage.toString() + "%", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
-                                              Icon(Icons.discount, color: Colors.blue,), 
-                                            ],  
-                                          ),
-                                        ]
-                                      ),
-                                    ),
-                                  ]
-                                );
-                              }
-                            );
-                          },
-                        ),   
-                      ],  
-                    ),
-                  ]
-                ),
-              ),
-              Padding
-              (
-                padding: EdgeInsets.only(left:10.0, right: 10.0, bottom: 10.0),
-                child:
-              Text(itemsList[i].description.toString()),
-              ),
-            ],
-          ),
-        // ),
+        color: Colors.yellow,
+        child: Text(itemsList[i].title),
       );
+      // return Card
+      // (
+      //   color: Colors.white60,
+      //   shape: RoundedRectangleBorder
+      //   (
+      //     borderRadius: BorderRadius.circular(20.0),
+      //   ),
+      //   // child: Padding
+      //   // (
+      //   //   padding: EdgeInsets.all(10.0),
+          
+      //     child: Column
+      //     (
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       children: 
+      //       [
+      //         Expanded
+      //         (
+      //           child: Container
+      //           (
+      //             alignment: Alignment.topCenter,
+      //             width: double.infinity,
+      //             // height: double.minPositive,
+      //             // height: 50,
+      //             decoration: BoxDecoration
+      //             (
+      //               borderRadius: BorderRadius.circular(20.0),
+      //             ),
+      //             child: Image.network
+      //             (
+      //               itemsList[i].thumbnail.toString(),
+      //               fit: BoxFit.fill,
+      //             ),
+      //           ),
+      //         ),
+      //         // Expanded
+      //         // (
+      //         //   child: Image.network
+      //         //   (
+      //         //     itemsList[i].thumbnail.toString(),
+      //         //     fit: BoxFit.fitWidth,
+      //         //   ),
+      //         // ),
+
+      //         //  Expanded
+      //         // (
+      //         //   child: Container
+      //         //   (
+      //         //     width: double.infinity,
+      //         //     decoration: BoxDecoration
+      //         //     (
+      //         //       borderRadius: BorderRadius.circular(20.0),
+      //         //     ),
+      //         //     child: Image.network
+      //         //     (
+      //         //       itemsList[i].thumbnail.toString(),
+      //         //       fit: BoxFit.fill,
+      //         //     ),
+      //         //   ),
+      //         // ),
+      //         Padding
+      //         (
+      //           padding: EdgeInsets.only(left:10.0, right: 10.0),
+      //           child:
+      //           Row
+      //           (
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: 
+      //             [
+                    
+      //               // Text(itemsList[i].title, style: TextStyle(fontWeight: FontWeight.bold)),
+      //               Text(itemsList[i].title.length > 20 ? '${itemsList[i].title.substring(0, 20)}...' : itemsList[i].title, style: TextStyle(fontWeight: FontWeight.bold)),
+      //               Row
+      //               (
+      //                 children: 
+      //                 [
+      //                   Text(itemsList[i].price.toString() + " USD", style: TextStyle(fontWeight: FontWeight.bold)),
+      //                   IconButton
+      //                   (
+      //                     icon: Icon(Icons.remove_red_eye_sharp),
+      //                     onPressed: ()
+      //                     {
+      //                       showModalBottomSheet<void>
+      //                       (
+                            
+      //                         shape: RoundedRectangleBorder
+      //                         (
+      //                           borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+      //                         ),
+      //                         constraints: BoxConstraints
+      //                         (
+      //                           // minWidth: double.infinity,
+      //                         ),
+      //                         // isScrollControlled: true,
+      //                         context: context,
+      //                         builder: (BuildContext context)
+      //                         {
+      //                           return Column
+      //                           (
+      //                             mainAxisSize: MainAxisSize.min,
+      //                             mainAxisAlignment: MainAxisAlignment.start,
+      //                             crossAxisAlignment: CrossAxisAlignment.start,
+      //                             children: 
+      //                             [
+      //                               Container
+      //                               (
+      //                                 margin: const EdgeInsets.symmetric(vertical: 20),
+      //                                 height: 150,
+      //                                 child: ListView
+      //                                 (
+      //                                   scrollDirection: Axis.horizontal,
+      //                                   children: itemsList[i].images != null && itemsList[i].images.isNotEmpty ? itemsList[i].images.map((imageInList)
+      //                                   {
+      //                                     return Card
+      //                                     (
+      //                                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
+      //                                       child: Container
+      //                                       (
+      //                                         decoration: BoxDecoration
+      //                                         (
+      //                                           borderRadius: BorderRadius.circular(20.0),
+      //                                         ),
+      //                                         child: Image.network(imageInList, fit: BoxFit.fitHeight, width: 150),
+      //                                       ),
+      //                                     );
+      //                                     // return Image.network(imageInList);
+      //                                   }).toList() : <Widget>[],
+      //                                 ),
+      //                               ),
+      //                               ListTile
+      //                               (
+      //                                 title: Text(itemsList[i].title,style: TextStyle(fontWeight: FontWeight.bold)),
+      //                                 subtitle: Text(itemsList[i].description.toString()),
+      //                               ),
+      //                               ListTile
+      //                               (
+      //                                 title: Text("\$" + itemsList[i].price.toString(),style: TextStyle(fontWeight: FontWeight.bold)),
+      //                               ),
+      //                               ListTile
+      //                               (
+      //                                 title: Row
+      //                                 (
+      //                                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //                                   children: 
+      //                                   [
+      //                                     Row
+      //                                     (
+      //                                       children: 
+      //                                       [
+      //                                         Icon(Icons.star, color: Colors.yellow,),
+      //                                         Text(itemsList[i].rating.toString(),),   
+      //                                       ],  
+      //                                     ),
+      //                                     Row
+      //                                     (
+      //                                       children: 
+      //                                       [
+      //                                         Text(itemsList[i].discountPercentage.toString() + "%", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold)),
+      //                                         Icon(Icons.discount, color: Colors.blue,), 
+      //                                       ],  
+      //                                     ),
+      //                                   ]
+      //                                 ),
+      //                               ),
+      //                             ]
+      //                           );
+      //                         }
+      //                       );
+      //                     },
+      //                   ),   
+      //                 ],  
+      //               ),
+      //             ]
+      //           ),
+      //         ),
+      //         Padding
+      //         (
+      //           padding: EdgeInsets.only(left:10.0, right: 10.0, bottom: 10.0),
+      //           child:
+      //         Text(itemsList[i].description.toString()),
+      //         ),
+      //       ],
+      //     ),
+      //   // ),
+      // );
     },
   );
 }
